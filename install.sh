b7a81a1ff1638e96eeaac88a6ed578c2bb7a73ee
luominglu <dissipator520@gmail.com>
2019-06-08 12:13:55 +0800

    init

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb 
+apt install -f
+npm install puppeteer --unsafe-perm=true
+npm install 
+npm start 

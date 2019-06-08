117f15062b509bbb4c38575b51da7b2ad8a3f899
luominglu <dissipator520@gmail.com>
2019-06-08 12:32:38 +0800

    update install file

--- README.md
+++ README.md
@@ -7,6 +7,26 @@
 
 本项目依赖node.js，因此使用之前，请先确保安装了最新稳定版本的node.js。
 
+How to install Node.js via binary archive on Linux?
+Unzip the binary archive to any directory you wanna install Node, I use /usr/local/lib/nodejs
+ VERSION=v10.15.0
+ DISTRO=linux-x64
+ sudo mkdir -p /usr/local/lib/nodejs
+ sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs 
+Set the environment variable ~/.profile, add below to the end
+# Nodejs
+VERSION=v10.15.0
+DISTRO=linux-x64
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
+Refresh profile
+. ~/.profile
+Test installation using
+$ node -v
+
+$ npm version
+
+$ npx -v
+
 
 ## 运行程序
 
--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
+wget https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-x64.tar.xz
+
+VERSION=v8.9.0
+DISTRO=linux-x64
+sudo mkdir -p /usr/local/lib/nodejs
+sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs 
+
+cat >~/.profile<<EOF
+VERSION=v8.9.0
+DISTRO=linux-x64
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
+EOF
+. ~/.profile
+$ node -v
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb 
 apt install -f
+rm -rf google-chrome-stable_current_amd64.deb node-v8.9.0-linux-x64.tar.xz
 npm install puppeteer --unsafe-perm=true
 npm install 
 npm start 
